d7e72b95d60c038e83a2d57b8378294e6c1a9f38
Lee Phuoc <phuoclx@nal.vn>
2019-05-17 16:33:03 +0700

    Update data fake of users table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,16 +16,22 @@ class DatabaseSeeder extends Seeder
 
         // $this->call(UserTableSeeder::class);
 
+        DB::table('users')->truncate();
+
         DB::table('users')->insert([
             [
                 'id' => 1,
                 'login_id' => 'user_1',
                 'password' => 'demo',
+                'created_at' => date('Y-m-d H:i:s', strtotime('now')),
+                'updated_at' => date('Y-m-d H:i:s', strtotime('now'))
             ],
             [
                 'id' => 2,
                 'login_id' => 'user_2',
                 'password' => 'demo',
+                'created_at' => date('Y-m-d H:i:s', strtotime('now')),
+                'updated_at' => date('Y-m-d H:i:s', strtotime('now'))
             ]
         ]);
 
