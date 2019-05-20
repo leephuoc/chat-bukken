716d98a743282421e45a330041fcd9bd96a077e6
Lee Phuoc <phuoclx@nal.vn>
2019-05-20 09:28:57 +0700

    Build route callback for webhook of LINE

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,5 +13,5 @@ Route::get('/home','HomeController@index');
 Route::get('/loginByLine', 'LoginController@loginByLine');
 Route::post('/loginByLine', 'LoginController@loginByLine');
 
-Route::get('/callback', 'ApiLineController@callback');
+//Route::get('/callback', 'ApiLineController@callback');
 Route::post('/callback', 'ApiLineController@callback');
